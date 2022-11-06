9ac79a210b2dad5dc6d1839712ffde71affe670f
Elyna Fock <elinayen2000@gmail.com>
2022-11-06 14:14:28 +0800

    Header of Index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,28 @@
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main className="font-light pt-12">
+      <div className="max-w-3xl m-auto">
+        <header className="grid grid-cols-4 gap-4">
+          <div className="col-span-3">
+            <h1 className="text-5xl font-bold py-2">Elyna Fock</h1>
+            <h2>Web Developer | UX Designer | Student</h2>
+            <p className="py-4">
+              Software Engineer student at{" "}
+              <a href="https://www.unimas.my/" target={"_blank"}>
+                UNIMAS
+              </a>
+              . Experience in Web Development, Figma, Photoshop and Illustrator.
+            </p>
+          </div>
+          <div className="col-span-1 flex justify-center	items-center	">
+            <img
+              src="/profile.jpg"
+              alt="Profile Picture"
+              className="rounded-full	w-36"
+            />
+          </div>
+        </header>
+      </div>
+    </main>
+  );
 }
