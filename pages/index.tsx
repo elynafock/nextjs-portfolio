e7a9a4efa2c6ced7a92867d80df4df3965d59e8f
Elyna Fock <elinayen2000@gmail.com>
2022-11-21 10:42:31 +0800

    Mute Unmute and Light Dark Feature

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
+import NavBar from "../components/core/NavBar";
 import Header from "../components/index/Header";
 import Skills from "../components/index/Skills";
 
 export default function Home() {
   return (
-    <main className="font-light pt-12">
-      <div className="max-w-3xl m-auto">
+    <main className="font-light pt-12 grid grid-cols-3">
+      <div className="max-w-3xl m-auto col-span-3 px-12 lg:col-span-2">
         <Header />
         <Skills />
+      </div>
+      <div className="hidden col-span-1 px-12 lg:block">
+        <NavBar />
       </div>
     </main>
   );
