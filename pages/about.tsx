01c695a5078e1c594a41050210a9de6cb7319b91
Elyna Fock <elinayen2000@gmail.com>
2022-12-08 11:36:26 +0800

    About Me Page

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,7 +2,43 @@ import React from "react";
 import AppLayout from "../components/core/AppLayout";
 
 const about = () => {
-  return <AppLayout>hi</AppLayout>;
+  return (
+    <AppLayout>
+      <article>
+        <h3 className="text-3xl font-bold py-2 mt-8 mb-4">About Me</h3>
+        <div className="gap-y-4 flex flex-col">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad natus
+            blanditiis facilis consequuntur inventore quas magnam accusantium,
+            recusandae, iste quis ab tenetur. Quia fugiat iste voluptas
+            voluptatem odio atque, libero earum omnis provident ipsa commodi
+            facilis maiores, iure voluptatibus saepe non dignissimos inventore
+            harum dolorem nostrum tempore sed debitis. Quaerat quis praesentium
+            minus vero ea culpa soluta consectetur nihil minima? Architecto
+            corporis exercitationem iste amet et ab officia explicabo ullam
+            dolorem laborum cupiditate tempore expedita vitae nesciunt
+            doloremque tempora, odit, dicta quo perferendis obcaecati quaerat
+            natus eaque nam sit. Itaque perspiciatis totam exercitationem dolor
+            veritatis, sapiente dignissimos distinctio ipsum accusantium.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad natus
+            blanditiis facilis consequuntur inventore quas magnam accusantium,
+            recusandae, iste quis ab tenetur. Quia fugiat iste voluptas
+            voluptatem odio atque, libero earum omnis provident ipsa commodi
+            facilis maiores, iure voluptatibus saepe non dignissimos inventore
+            harum dolorem nostrum tempore sed debitis. Quaerat quis praesentium
+            minus vero ea culpa soluta consectetur nihil minima? Architecto
+            corporis exercitationem iste amet et ab officia explicabo ullam
+            dolorem laborum cupiditate tempore expedita vitae nesciunt
+            doloremque tempora, odit, dicta quo perferendis obcaecati quaerat
+            natus eaque nam sit. Itaque perspiciatis totam exercitationem dolor
+            veritatis, sapiente dignissimos distinctio ipsum accusantium.
+          </p>
+        </div>
+      </article>
+    </AppLayout>
+  );
 };
 
 export default about;
