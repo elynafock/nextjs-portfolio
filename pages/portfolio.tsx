074fe5ebac6235b2cc6091a4cf94d0cb1eb69d7f
Elyna Fock <elinayen2000@gmail.com>
2022-12-09 15:47:53 +0800

    portfolio projects 6/6 and design

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -30,16 +30,21 @@ const portfolio = () => {
       image: "dusted.png",
       description:
         "Take responsibility in research, design and prototyping high-fidelity design",
+      link: "https://www.figma.com/file/m0zJK2QmB4NDgyExr95Vls/Untitled?node-id=0%3A1&t=Over73eJSJCMJGsa-0",
     },
     {
-      name: "Break-fast",
-      image: "portfolio.jpg",
-      description: "Help people",
+      name: "Break-Fast",
+      image: "break-fast.png",
+      description:
+        "Take responsibility in research, design and prototyping high-fidelity design",
+      link: "https://www.figma.com/file/N2xdUoNKSqprC3Kg0MpT9Y/Intelligent-System-Assignment?node-id=0%3A1&t=23peKe9MlEtrAKc0-0",
     },
     {
-      name: "Digital Illustration + Firebase Project",
-      image: "portfolio.jpg",
-      description: "Digital Illustration + Firebase Project",
+      name: "Elyna's Collection",
+      image: "elynas-collection.png",
+      description:
+        "This website is built to store digital illustrations drawn using SveletKit and Firebase",
+      link: "https://elynascollection.netlify.app/",
     },
   ];
   return (
@@ -48,19 +53,24 @@ const portfolio = () => {
         <h3 className="text-3xl font-bold py-2 mt-8 mb-4">Portfolio</h3>
         <div className="grid grid-cols-1 gap-8">
           {projects.map((project) => (
-            <div key={project.name} className="flex flex-col gap-y-1">
+            <a
+              href={project.link}
+              target="_blank"
+              key={project.name}
+              className="flex flex-col gap-y-1 hover:no-underline shadow-lg hover:shadow-2xl hover:scale-105 transition-all duration-500 shadow-neutral-200 rounded-lg"
+            >
               <img
                 src={project.image}
                 alt={project.name}
-                className="aspect-video object-cover"
+                className="aspect-video object-cover rounded-t-lg"
               />
-              <div>
+              <div className="px-4 pt-2 pb-4">
                 <h4 className="text-xl font-semibold">{project.name}</h4>
-                <p className="font-light text-xs text-gray-500">
+                <p className="font-light text-xs text-gray-500  ">
                   {project.description}
                 </p>
               </div>
-            </div>
+            </a>
           ))}
         </div>
       </article>
